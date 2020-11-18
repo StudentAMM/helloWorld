b43be175f2a137d48c215365dfb2189dd487229d
Коротких А.А <evolenteam@gmail.com>
2020-11-18 19:56:31 +0300

    теперь можно изменять выводимое сообщение с помощью аргументов

--- src/main/java/hw/HelloWorld.java
+++ src/main/java/hw/HelloWorld.java
@@ -2,7 +2,11 @@ package hw;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        if(args.length>0){
+            System.out.println(args[0]);
+        }
+        else
+            System.out.println("Hello world!");
     }
 }
 
